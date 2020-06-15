a03449410ededdae1cf47f65a1ab8443a9091955
郭洪铭 <478476589@qq.com>
2020-06-15 11:16:16 +0800

    部署

--- pc_website/src/views/Introduce.vue
+++ pc_website/src/views/Introduce.vue
@@ -2,7 +2,7 @@
   <div class="Home">
     <img class="top_img" src="../assets/introduce_banner.jpg" :style="{height:`${bannerHeight}px`}" />
     <Fast title="吉易慧农概述">
-      <el-row class="text">
+      <el-row class="text" style="padding: 0 50px">
         <el-col class="left_image" :span="8">
           <el-image :src="leftImage" fit="cover"></el-image>
         </el-col>
@@ -12,7 +12,7 @@
           >吉易慧农致力于依托互联网技术和先进的商业服务理念，打造集农资销售服务、商业推广、教育培训、农技服务和资源整合于一体的农业种植全产业链互联网服务平台，实施新型职业农民培育计划，改革创新农业产业生态，服务农业种植领域全过程。</p>
         </el-col>
       </el-row>
-      <el-row>
+      <el-row style="padding: 0 50px;">
         <p
           class="i_p top"
         >吉易慧农平台提供的互联网技术服务和农业科技服务，稳固建立现代农业经理人、会员、商户、科研机构的合作伙伴关系，为每一位愿意凭籍吉易慧农实现人生目标的合作者提供持续的发展助力。吉易慧农坚持科技兴农、智慧兴农，以促进农户和现代农业发展有机衔接、提升农业生态为导向，以线上网络平台和线下培育新型职业农民服务体系为载体，以投身国家乡村振兴战略、维护国家粮食安全为终极目标。</p>
