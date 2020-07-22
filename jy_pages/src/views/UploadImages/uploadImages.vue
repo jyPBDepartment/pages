f45d2b05792d863c40b463b0cea179b2be5b694d
zhouguohui <zhouguohui0328@163.com>
2020-07-22 17:40:06 +0800

    新增图片上传实例功能

--- jy_pages/src/views/UploadImages/uploadImages.vue
+++ jy_pages/src/views/UploadImages/uploadImages.vue
@@ -57,6 +57,7 @@ export default {
       return;
     },
     uploadSuccess(response, file, fileList) {
+        console.log(response.url)
       this.flag = true;
       this.imgUrl = response.url;
     },
