bff7fd75a47cc81be1d3ecb02d339ddd6ab56e6a
郭洪铭 <478476589@qq.com>
2020-07-27 11:25:23 +0800

    在pc端判断浏览器标识 如果为移动端 跳转到 移动端地址

--- jiyi-for-web/src/App.vue
+++ jiyi-for-web/src/App.vue
@@ -22,9 +22,6 @@ export default {
     if (flag) {
       //移动端
       window.location.href='http://www.jiyinongye.com/phone/#/';
-    } else {
-      //pc端
-      window.location.href='http://www.jiyinongye.com/web/#/';
     }
   },
 };
