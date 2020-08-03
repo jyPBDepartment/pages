3523525e4ed1b7c84787630274ac99615697ec75
JXNxixi <1084562297@qq.com>
2020-08-03 17:53:55 +0800

    修改权限管理功能

--- jy_fcs_pages/src/views/powerInfo/updatePowerInfo.vue
+++ jy_fcs_pages/src/views/powerInfo/updatePowerInfo.vue
@@ -10,31 +10,17 @@
     :close-on-press-escape="false"
   >
     <!-- 插槽区 -->
-    <slot>
+     <slot>
       <el-form :rules="rules" ref="powerInfoForm" :model="powerInfoForm" label-width="100px">
         <el-form-item label="权限名称" prop="jurName">
-          <el-input
-            type="text"
-            v-model="powerInfoForm.jurName"
-            placeholder="请输入权限名称"
-            style=" width:70%;"
-          ></el-input>
+          <el-input type="text" v-model="powerInfoForm.jurName" placeholder="请输入权限名称" style=" width:70%;"></el-input>
         </el-form-item>
-        <el-form-item label="权限编码" prop="jurCode">
-          <el-input
-            type="text"
-            v-model="powerInfoForm.jurCode"
-            placeholder="请输入权限编码"
-            style=" width:70%;"
-          ></el-input>
+        <el-form-item label="权限编码" prop="jurCode" >
+           <el-input type="text" v-model="powerInfoForm.jurCode" placeholder="请输入权限编码" style=" width:70%;"></el-input>
         </el-form-item>
-        <el-form-item label="上级权限编码" prop="subJurCode">
-          <el-select
-            type="text"
-            v-model="powerInfoForm.subJurCode"
-            placeholder="请输入上级权限编码"
-            style=" width:70%;"
-          >
+      
+         <el-form-item label="上级权限编码" prop="subJurCode" >
+          <el-select type="text" v-model="powerInfoForm.subJurCode" placeholder="请输入上级权限编码" style=" width:70%;" >
             <el-option
               v-for="item in powerOptions"
               :key="item.value"
@@ -43,31 +29,16 @@
             ></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="状态" prop="auditStatus">
-          <el-input
-            type="text"
-            v-model="powerInfoForm.auditStatus"
-            placeholder="请输入状态"
-            style=" width:70%;"
-          ></el-input>
+          <el-form-item label="状态" prop="auditStatus">
+          <el-input type="text" v-model="powerInfoForm.auditStatus" placeholder="请输入状态" style=" width:70%;"></el-input>
         </el-form-item>
+         
       </el-form>
     </slot>
     <!-- 按钮区 -->
     <span slot="footer">
-      <el-button
-        type="success"
-        icon="el-icon-check"
-        @click="updatePowerInfo()"
-        style="background-color:#409EFF;border-color:#409EFF;color:white;font-size:12px;width:105px;height:42px;"
-      >保存</el-button>
-      <el-button
-        type="danger"
-        icon="el-icon-close"
-        @click="close"
-        size="medium"
-        style="background-color:white;border-color:black;color:black;font-size:12px;width:105px;height:42px;"
-      >关闭</el-button>
+      <el-button type="success" icon="el-icon-check" @click="updatePowerInfo()" style="background-color:#409EFF;border-color:#409EFF;color:white;font-size:12px;width:105px;height:42px;">保存</el-button>
+      <el-button type="danger" icon="el-icon-close" @click="close"  size="medium" style="background-color:white;border-color:black;color:black;font-size:12px;width:105px;height:42px;">关闭</el-button>
     </span>
   </el-dialog>
 </template>
@@ -75,119 +46,117 @@
 import ApiPath from "@/api/ApiPath.js";
 import api from "@/axios/api.js";
 export default {
+   
   inject: ["reload"],
   props: {
     show: {
       type: Boolean,
-      default: false,
+      default: false
     },
     title: {
       type: String,
-      default: "对话框",
+      default: "对话框"
     },
     transPowerInfoId: {
       type: String,
-      default: "对话框",
-    },
+      default: "对话框"
+    }
   },
   data() {
     return {
       localShow: this.show,
-      powerInfoForm: {
-        jurName: "",
-        jurCode: "",
-        subJurCode: "",
-        auditStatus: "",
+     powerInfoForm: {
+        
+        jurName:"",
+        jurCode:"",
+        subJurCode:"",
+        auditStatus:"",
+        
       },
-      powerOptions: [],
+     
+     powerOptions:[],
       // rules表单验证
       rules: {
-        jurName: [
-          { required: true, message: "请输入权限名称", trigger: "blur" },
-        ],
+       jurName: [{ required: true, message: "请输入权限名称", trigger: "blur" }],
         jurCode: [
           { required: true, message: "请输入权限编码", trigger: "blur" },
         ],
-        auditStatus: [
-          { required: true, message: "请输入状态", trigger: "blur" },
-        ],
-      },
+        auditStatus:[{required: true, message: "请输入状态", trigger: "blur"}]
+      }
     };
   },
   watch: {
     show(val) {
       this.localShow = val;
     },
-    //根据Id查询用户信息
     transPowerInfoId(val) {
+     
       let params = {
-        id: val,
+        id: val
       };
-      api.testAxiosGet(ApiPath.url.powerInfoFindById, params).then((res) => {
+
+      //根据Id查询用户信息
+      api.testAxiosGet(ApiPath.url.powerInfoFindById, params).then(res => {
         if (res.status == 0) {
           this.powerInfoForm = res.data;
         }
       });
-    },
+    }
   },
-
+ 
   mounted() {
-    this.findContext();
+     this.findContext();
   },
   methods: {
     //下拉列表显示
-    findContext: function () {
-      let params = {
-        subJurCode: this.powerInfoForm.subJurCode,
-      };
-      api
-        .testAxiosGet(ApiPath.url.updatePower, params)
-        .then((res) => {
-          if (res.status == "0") {
-            for (let i = 0; i < res.data.length; i++) {
-              this.powerOptions.push({
-                value: res.data[i]["id"],
-                label: res.data[i]["jurCode"],
-              });
+    findContext: function() {
+        let params = {
+        subJurCode :this.powerInfoForm.subJurCode
+        };
+        api
+          .testAxiosGet(ApiPath.url.updatePower, params)
+          .then(res => {
+            if (res.status == "0") {
+              for (let i = 0; i < res.data.length; i++) {
+                this.powerOptions.push({
+                  value: res.data[i]["id"],
+                  label: res.data[i]["jurCode"]
+                });
+              }
             }
-          }
-        })
-        .catch(function (error) {});
+          })
+          .catch(function(error) {
+          });
     },
 
     //修改权限信息
-    updatePowerInfo: function () {
-      if (this.editForm.jurName != "" && this.editForm.jurCode !="" && this.editForm.auditStatus !="") {
+    updatePowerInfo: function() {
       let params = {
-        powerInfoEntity: this.powerInfoForm,
+        powerInfoEntity: this.powerInfoForm
       };
-      api
-        .testAxiosGet(ApiPath.url.updatePowerInfo, params)
-        .then((res) => {
-          this.$message.success(res.message);
 
-          this.reload();
-
-          this.close();
-        })
-        .catch((err) => {
-          this.$message.error(err.data);
-        });
-      this.powerInfoForm.updateUser = localStorage.getItem("userInfo");
-    }else {
-        this.$alert("权限名称,权限编码,状态不能为空！", "提示", {
-          confirmButtonText: "确定",
-        });
-      }
-   
-    }
-  },
-   close: function () {
+      api.testAxiosGet(ApiPath.url.updatePowerInfo, params).then(res => {
+       
+       
+       this.$message.success(res.message);
+        
+        this.reload();
+        
+        this.close();
+       
+          
+      }) .catch(err => {
+                this.$message.error(err.data);
+              });
+             this.powerInfoForm.updateUser=localStorage.getItem("userInfo")
+    },
+    close: function() {
       this.$emit("close");
     },
-    beforeClose: function () {
+    beforeClose: function() {
       this.close();
-    },
+    }
+  }
 };
 </script>
 
@@ -195,7 +164,7 @@ export default {
 .el-form {
   padding-left: 115px;
 }
-.el-button {
+.el-button{
   border: none;
 }
 </style>
