6778ad97141316070fc16b6e039344af8933b6d2
JXNxixi <1084562297@qq.com>
2020-08-18 14:29:31 +0800

    粮食收购，出售，出租农机

--- jiyi-h5/pages/grain/space.vue
+++ jiyi-h5/pages/grain/space.vue
@@ -1,27 +1,45 @@
+<!-- 注：param==0为粮食收购-出售详情，param==1为出售农机-出售详情,param==2为出租农机-出售详情 -->
 <template>
 	<view>
 
-		<!-- <view v-if="!param" class=""> -->
+
 		<view class="init">
 			<image class="agri-img" src="@/static/logo.png" mode=""></image>
-			<text class="title">{{title}}aaa</text>
-			<text class="price">¥{{price}}元/公斤</text>
-			<view class="tagGrain" v-for="(item, index) in item" :key="index">
-				{{item.brige}}aaa
+
+			<text v-if="param == 2 " class="sell-bg">
+				出租
+			</text>
+			<text v-if="param == 2 " class="sell-title">{{title}}aaa</text>
+
+			<view v-if="param == 2 " class="sell-cool">
+				<image class="sell-img" src="@/static/logo.png" mode=""></image>
+				<text class="sell-text">分享</text>
 			</view>
-			
-			<view class="type">
-				<text>类别：{{corn}}aaa</text>
-			</view>
-			<view class="two">
-				<text>地址:{{address}}aaa</text>
-				<view>
-					<image class="icon" style="width: 50rpx;height: 50rpx;" src="@/static/logo.png" mode=""></image>
-					<text>{{name}}aaa</text>
+
+			<text v-if="param == 0 || param ==1" class="title">{{title}}aaa</text>
+			<text v-if="param == 0" class="price">¥{{price}}元/公斤</text>
+			<text v-if="param == 1 " class="price">¥{{price}}55555.00</text>
+			<text v-if="param == 2 " class="price">¥{{price}}元/亩</text>
+
+			<view class="tag">
+				<view class="tagGrain">
+					{{item.brige}}aaa
+				</view>
+				<view class="tagG">
+					{{item.brige}}aaa
 				</view>
 			</view>
+
+			<view class="type">
+				<text v-if="param == 0" class="type-text">类别：{{corn}}aaa</text>
+				<text v-if="param == 1 || param == 2 " class="type-agri">型号：{{corn}}bbb</text>
+				<text v-if="param == 1 || param == 2 " class="type-agri">货号：{{corn}}bbb</text>
+
+				<text v-if="param == 0" class="type-map">{{address}}吉林省榆树市五棵松市</text>
+				<text v-if="param == 1 || param == 2 " class="type-mapAgri">{{address}}长春公主岭</text>
+			</view>
 			<view class="tel">
-				<text>
+				<text class="phone">
 					电话：{{phone}}11111
 				</text>
 			</view>
@@ -35,7 +53,11 @@
 	export default {
 		data() {
 			return {
-				param: 0,
+				param: {
+					param: Number,
+					default: 0
+				},
+				// param: 0,
 				item: {
 					type: Array,
 					default: () => {
@@ -60,38 +82,121 @@
 		justify-content: center;
 		margin-left: 24rpx;
 
+		.sell-bg {
+			border: 1px solid rgba(255, 0, 0, 1);
+			background-color: rgba(255, 0, 0, 1);
+			width: 70rpx;
+			line-height: 40rpx;
+			text-align: center;
+			font-size: 24rpx;
+			color: #FFF;
+		}
+
+		.sell-title {
+			font-size: 30rpx;
+			margin-bottom: 24rpx;
+			margin-top: -45rpx;
+			margin-left: 90rpx;
+		}
+
+		.sell-cool {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			margin-left: 650rpx;
+			margin-top: -70rpx;
+
+			.sell-img {
+				width: 46rpx;
+				height: 46rpx;
+				background-color: #fff;
+			}
+
+			.sell-text {
+				font-size: 24rpx;
+				color: rgba(153, 153, 153, 1);
+
+			}
+		}
+
 		.title {
 			font-size: 30rpx;
-			
 			margin-bottom: 24rpx;
 		}
 
 		.price {
 			font-size: 42rpx;
-			
 			margin-bottom: 24rpx;
 		}
 
-		.tagGrain {
+		.tag {
 			display: flex;
 			flex-direction: row;
-			justify-content: center;
-			width: 100rpx;
-			border-radius: 16rpx;
-			border: 1px solid rgba(255, 87, 51, 1);
-			color: rgba(255, 87, 51, 1);			
-			text-align: center;
-			font-size: 24rpx;
-			margin-bottom: 24rpx;
+			align-items: center;
+
+			.tagGrain {
+				margin-right: 20rpx;
+				width: 100rpx;
+				border-radius: 16rpx;
+				border: 1px solid rgba(255, 87, 51, 1);
+				color: rgba(255, 87, 51, 1);
+				text-align: center;
+				font-size: 24rpx;
+				margin-bottom: 36rpx;
+			}
+
+			.tagG {
+				margin-right: 20rpx;
+				width: 100rpx;
+				border-radius: 16rpx;
+				border: 1px solid rgba(24, 144, 255, 1);
+				color: rgba(24, 144, 255, 1);
+				text-align: center;
+				font-size: 24rpx;
+				margin-bottom: 36rpx;
+			}
 		}
 
+		.type {
+			display: flex;
+			margin-bottom: 20rpx;
+
+			.type-text {
+				font-size: 24rpx;
+				color: rgba(153, 153, 153, 1);
+			}
+
+			.type-agri {
+				font-size: 24rpx;
+				color: rgba(153, 153, 153, 1);
+				margin-right: 100rpx;
+			}
+
+			.type-map {
+				font-size: 24rpx;
+				color: rgba(153, 153, 153, 1);
+				margin-left: 300rpx;
+			}
+
+			.type-mapAgri {
+				font-size: 24rpx;
+				color: rgba(153, 153, 153, 1);
+				margin-left: 80rpx;
+			}
+		}
+
+		.tel {
+			.phone {
+				font-size: 24rpx;
+				color: rgba(153, 153, 153, 1);
+			}
+		}
 
 		.agri-img {
-			margin-left: -24rpx;
-			width: 750rpx;
+			margin-left: -89rpx;
+			margin-bottom: -40rpx;
+			width: 880rpx;
 			height: 750rpx;
 		}
 	}
-
-	
 </style>
