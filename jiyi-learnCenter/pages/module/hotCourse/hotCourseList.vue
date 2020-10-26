281193128eb8d8ef6fade5c87d15b95ffb95564c
JXNxixi <1084562297@qq.com>
2020-10-26 14:23:20 +0800

    热门课程 学习考试分类加载页面

--- jiyi-learnCenter/pages/module/hotCourse/hotCourseList.vue
+++ jiyi-learnCenter/pages/module/hotCourse/hotCourseList.vue
@@ -2,22 +2,50 @@
 	<view class="content">
 		<!-- 头部 -->
 		<view class="head">
-				<view class="backArrow" @click="backTo">
-					<u-icon name="arrow-left" color="#333" size="32"></u-icon>
-				</view> 
-				<view class="title">热门课程</view>
+			<view class="backArrow" @click="backTo">
+				<u-icon name="arrow-left" color="#333" size="32"></u-icon>
+			</view>
+			<view class="title">热门课程</view>
 		</view>
-		
+
 		<!-- 搜索 -->
 		<view class="select">
 			<view>
-				<u-search placeholder="搜索标题或标签" bg-color="#fff" :show-action="false" input-align="left" shape="square" margin="30rpx 0rpx 0rpx 20rpx"  maxlength="16" class="input" v-model="title"></u-search>
+				<u-search placeholder="搜索标题或标签" bg-color="#fff" :show-action="false" input-align="left" shape="square" margin="30rpx 0rpx 0rpx 20rpx"
+				 maxlength="16" class="input" v-model="searchTitle"></u-search>
 			</view>
 		</view>
-		<Screen :listIndex="listIndex" @select="select" :condition="condition" ></Screen>	
+				<Screen :listIndex="listIndex" @select="select" :condition="condition" ></Screen>
+		<!-- <view>
+			<u-tabs-swiper ref="uTabs" :list="list" :current="current" @change="tabsChange" :is-scroll="false" swiperWidth="750"></u-tabs-swiper>
+		</view>
+		<swiper :current="swiperCurrent" @transition="transition" @animationfinish="animationfinish">
+			<swiper-item v-for="(listItem, index) in list" :key="index">
+				<scroll-view scroll-y class="scrollStyle">
+					<mescroll-body ref="mescrollRef" @init="mescrollInit" @down="downCallback" @up="upCallback" :down="downOption" :up="upOption">-->
+						<!-- 列表详情 -->
+					<!-- 	<view class="list" v-if="listItem.code == item.type || listItem.name == '全部'" v-for="(item, index) in industryCaseList"
+						 :key="index" @click="industryCaseJump('./index')">
+							<view class="listOne">
+								<view class="img">
+									<image class="listImg" :src="item.url"></image>
+								</view>
+								<view class="listText">{{item.word}}</view>
+							</view>
+							<view class="listSen">
+								<view class="bride">{{item.sum}}人已学</view>
+							</view>
+							<view>
+								<u-line class="underline"></u-line>
+							</view>
+						</view>
+					</mescroll-body>
+				</scroll-view>
+			</swiper-item>
+		</swiper> -->
 		<mescroll-body ref="mescrollRef" @init="mescrollInit" @down="downCallback" @up="upCallback" :down="downOption" :up="upOption">
 		<!-- 列表详情 -->
-		<view class="list" v-for="(item, index) in industryCaseList" :key="index"  @click="industryCaseJump('./index')">
+			<view class="list" v-for="(item, index) in industryCaseList" :key="index"  @click="industryCaseJump('./index')">
 			<view class="listOne">
 				<view class="img">
 					<image class="listImg" :src="item.url"></image>
@@ -33,23 +61,23 @@
 		</view>
 		</mescroll-body>
 	</view>
-	
+
 </template>
 
 <script>
 	import Screen from '@/pages/module/components/screen';
 	import MescrollMixin from "@/components/mescroll-uni/mescroll-mixins.js";
-	export default{
+	export default {
 		mixins: [MescrollMixin], // 使用mixin (在main.js注册全局组件)
 		components: {
 			Screen
 		},
-		data(){
-			return{
+		data() {
+			return {
 				mescroll: null, // mescroll实例对象 (此行可删,mixins已默认)
 				// 下拉刷新的配置(可选, 绝大部分情况无需配置)
 				downOption: {
-				
+
 				},
 				// 上拉加载的配置(可选, 绝大部分情况无需配置)
 				upOption: {
@@ -61,55 +89,161 @@
 						tip: '暂无相关数据'
 					}
 				},
-				condition: [
-					{code:"",name: '全部'},
-					{code:0,name: '农技'}, 
-					{code:1,name: '财经'},
-					{code:2,name: '管理'},
-					{code:3,name: '推广'}
-					],
-					listIndex:0,
-					
-					title:'',
-					industryCaseList:[
-						{url:"http://60.205.246.126/images/2020/10/12/1602481525521725.jpg",word:"人的欲望永无止尽 就像我一开始 只是想要知道你的名字",sum:"1314"},
-						{url:"http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",word:"培养农业职业经理人，是长春市今年农业战线重点改革之一，也是经济与生态体制......",sum:"1314"},
-						{url:"http://60.205.246.126/images/2020/10/12/1602481543327363.jpg",word:"刮奖刮出一个谢字就足够了。",sum:"1314"},
-						{url:"http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",word:"你眼里有全宇宙 胜过我见过的所有山川河流",sum:"1314"},
-						{url:"http://60.205.246.126/images/2020/10/12/1602481564370430.jpg",word:"孺子含辛，隐忍不嗔。",sum:"1314"},
-						{url:"http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",word:"加油呀,你是比自己想象的要好得多的人。",sum:"1314"},
-						{url:"http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",word:"加油呀,你是比自己想象的要好得多的人。",sum:"1314"},
-						{url:"http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",word:"加油呀,你是比自己想象的要好得多的人。",sum:"1314"},
-						{url:"http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",word:"加油呀,你是比自己想象的要好得多的人。",sum:"1314"},
-						{url:"http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",word:"加油呀,你是比自己想象的要好得多的人。",sum:"1314"},
-						{url:"http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",word:"加油呀,你是比自己想象的要好得多的人。",sum:"1314"},
-						{url:"http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",word:"加油呀,你是比自己想象的要好得多的人。",sum:"1314"},						
-						{url:"http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",word:"加油呀,你是比自己想象的要好得多的人。",sum:"1314"},
-						{url:"http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",word:"加油呀,你是比自己想象的要好得多的人。",sum:"1314"}
-						
-					],
-					formInline: {
-						page: 1,
-						limit: 10,
-						varLable: "",
-						varName: "",
-						currentPage: 1,
-						pageSize: 10,
-						total: 10,
+
+				listIndex: 0,
+
+				searchTitle: '',
+				condition: [{
+						code: "",
+						name: '全部'
 					},
-					upOption: {
-						page: {
-							size: 10 // 每页数据的数量,默认10
-						},
-						noMoreSize: 10, // 配置列表的总数量要大于等于5条才显示'-- END --'的提示
-						empty: {
-							tip: '暂无相关数据'
-						}
+					{
+						code: 0,
+						name: '农技'
 					},
-				}
+					{
+						code: 1,
+						name: '财经'
+					},
+					{
+						code: 2,
+						name: '管理'
+					},
+					{
+						code: 3,
+						name: '推广'
+					}
+				],
+				// 因为内部的滑动机制限制，请将tabs组件和swiper组件的current用不同变量赋值
+				current: 0, // tabs组件的current值，表示当前活动的tab选项
+				swiperCurrent: 0, // swiper组件的current值，表示当前那个swiper-item是活动的
+				industryCaseList: [{
+						type: "0",
+						url: "http://60.205.246.126/images/2020/10/12/1602481525521725.jpg",
+						word: "0人的欲望永无止尽 就像我一开始 只是想要知道你的名字",
+						sum: "1314"
+					},
+					{
+						type: "1",
+						url: "http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",
+						word: "1培养农业职业经理人，是长春市今年农业战线重点改革之一，也是经济与生态体制......",
+						sum: "1314"
+					},
+					{
+						type: "0",
+						url: "http://60.205.246.126/images/2020/10/12/1602481543327363.jpg",
+						word: "0刮奖刮出一个谢字就足够了。",
+						sum: "1314"
+					},
+					{
+						type: "1",
+						url: "http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",
+						word: "1你眼里有全宇宙 胜过我见过的所有山川河流",
+						sum: "1314"
+					},
+					{
+						type: "1",
+						url: "http://60.205.246.126/images/2020/10/12/1602481564370430.jpg",
+						word: "1孺子含辛，隐忍不嗔。",
+						sum: "1314"
+					},
+					{
+						type: "0",
+						url: "http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",
+						word: "0加油呀,你是比自己想象的要好得多的人。",
+						sum: "1314"
+					},
+					{
+						type: "2",
+						url: "http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",
+						word: "2加油呀,你是比自己想象的要好得多的人。",
+						sum: "1314"
+					},
+					{
+						type: "2",
+						url: "http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",
+						word: "2加油呀,你是比自己想象的要好得多的人。",
+						sum: "1314"
+					},
+					{
+						type: "2",
+						url: "http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",
+						word: "2加油呀,你是比自己想象的要好得多的人。",
+						sum: "1314"
+					},
+					{
+						type: "3",
+						url: "http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",
+						word: "3加油呀,你是比自己想象的要好得多的人。",
+						sum: "1314"
+					},
+					{
+						type: "3",
+						url: "http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",
+						word: "3加油呀,你是比自己想象的要好得多的人。",
+						sum: "1314"
+					},
+					{
+						type: "3",
+						url: "http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",
+						word: "3加油呀,你是比自己想象的要好得多的人。",
+						sum: "1314"
+					},
+					{
+						type: "3",
+						url: "http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",
+						word: "3加油呀,你是比自己想象的要好得多的人。",
+						sum: "1314"
+					},
+					{
+						type: "3",
+						url: "http://60.205.246.126/images/2020/10/12/1602481589403089.jpg",
+						word: "3加油呀,你是比自己想象的要好得多的人。",
+						sum: "1314"
+					}
+
+				],
+				formInline: {
+					page: 1,
+					limit: 10,
+					varLable: "",
+					varName: "",
+					currentPage: 1,
+					pageSize: 10,
+					total: 10,
+				},
+				upOption: {
+					page: {
+						size: 10 // 每页数据的数量,默认10
+					},
+					noMoreSize: 10, // 配置列表的总数量要大于等于5条才显示'-- END --'的提示
+					empty: {
+						tip: '暂无相关数据'
+					}
+				},
+			}
 		},
-		
-		methods:{
+
+		methods: {
+			// tabs通知swiper切换
+			tabsChange(index) {
+				this.swiperCurrent = index;
+			},
+			// swiper-item左右移动，通知tabs的滑块跟随移动
+			transition(e) {
+				let dx = e.detail.dx;
+				this.$refs.uTabs.setDx(dx);
+			},
+			// 由于swiper的内部机制问题，快速切换swiper不会触发dx的连续变化，需要在结束时重置状态
+			// swiper滑动结束，分别设置tabs和swiper的状态
+			animationfinish(e) {
+				let current = e.detail.current;
+				this.$refs.uTabs.setFinishCurrent(current);
+				this.swiperCurrent = current;
+				this.current = current;
+			},
+
+
 			/*mescroll组件初始化的回调,可获取到mescroll对象 (此处可删,mixins已默认)*/
 			mescrollInit(mescroll) {
 				this.mescroll = mescroll;
@@ -122,14 +256,13 @@
 			upCallback(page) {
 				let pageNum = page.num; // 页码, 默认从1开始
 				let pageSize = page.size; // 页长, 默认每页10条
-				
+
 			},
 			backTo() {
-				uni.navigateBack({
-				})
+				uni.navigateBack({})
 			},
 			// 行业案例跳转详情页面
-			industryCaseJump(url){
+			industryCaseJump(url) {
 				uni.navigateTo({
 					url: url
 				})
@@ -140,72 +273,86 @@
 				this.downCallback()
 			}
 		}
-		
+
 	}
 </script>
 
 <style lang="scss" scoped>
-	.content{
+	.content {
 		display: flex;
 		flex-direction: column;
 		background-color: #F8F8F8;
-		.head{
+
+		.head {
 			display: flex;
 			margin-top: 15rpx;
 			margin-left: 20rpx;
-			.backArrow{
+
+			.backArrow {
 				margin-top: 6rpx;
 			}
-			.title{
+
+			.title {
 				margin-left: 250rpx;
 				font-size: 32rpx;
 			}
 		}
-		.select{
+		uni-swiper {
+			height:1600rpx;
+		}
+		.select {
 			display: flex;
 			height: 120rpx;
-			background-color:#C9C9CE;
+			background-color: #C9C9CE;
 			margin-top: 20rpx;
-			.input{
+
+			.input {
 				width: 700rpx;
-				
+
 			}
-			.uni-input-placeholder{
+
+			.uni-input-placeholder {
 				margin-left: 200rpx;
 			}
 		}
-		.list{
+
+		.list {
 			display: flex;
 			flex-direction: column;
 			background-color: #fff;
-			.listOne{
+
+			.listOne {
 				display: flex;
 				margin-top: 10rpx;
-				.img{
-					.listImg{
+
+				.img {
+					.listImg {
 						width: 220rpx;
 						height: 160rpx;
 						margin: 10rpx 10rpx;
 					}
 				}
-				.listText{
+
+				.listText {
 					font-size: 26rpx;
-					margin:15rpx 10rpx 0rpx 20rpx;
+					margin: 15rpx 10rpx 0rpx 20rpx;
 				}
 			}
-			.listSen{
-				
-				.bride{
+
+			.listSen {
+
+				.bride {
 					font-size: 22rpx;
 					color: #BFBFBF;
 					margin-left: 600rpx;
 					margin-top: -50rpx;
 				}
 			}
-			.underline{
+
+			.underline {
 				border: 2rpx solid #BBBBBB;
 			}
 		}
-		
+
 	}
 </style>
--- jiyi-learnCenter/pages/module/studyExamination/index.vue
+++ jiyi-learnCenter/pages/module/studyExamination/index.vue
@@ -11,10 +11,14 @@
 		<u-image width="100%" height="190rpx" :src="src"></u-image>
 		<!--  考试列表 -->
 		<u-row>
-			<u-col span="5" v-for="(item,key) in studyExaminationList" :key="key" 
-			class="se-col" :style="item.backgroundColor" @click="linkTo(item.levelCode)">
+			<u-col span="5" v-for="(item,key) in studyExaminationList" :key="key" class="se-col" :style="item.backgroundColor"
+			 @click="linkTo(item.levelCode,item.pass)">
+			 <!-- 角标 -->
+				 <view class="brage" v-if="item.pass=='0'">
+					<text>已通过</text>
+				 </view>
 				<view class="se-text" :style="item.levelColor">{{item.levelName}}</view>
-				<view style="text-align: center;margin-top: 12rpx;">{{item.title}}</view>
+				<view style="text-align: center;margin-top: 18rpx;font-size: 24rpx;font-weight: bold;">{{item.title}}</view>
 			</u-col>
 		</u-row>
 	</view>
@@ -26,36 +30,42 @@
 			return {
 				src: 'http://60.205.246.126/images/2020/09/29/1601348332838540.png',
 				studyExaminationList: [{
-						levelCode:'0',
+						levelCode: '0',
 						levelName: '初级',
 						title: '初级经理人考试',
-						levelColor: 'background-color: orange',
-						backgroundColor: 'background-color: #FCBD71'
+						levelColor: 'background-color: #fcca00',
+						backgroundColor: 'background-color: #fee78c',
+						pass:'0'
 					},
 					{
-						levelCode:'1',
+						levelCode: '1',
 						levelName: '中级',
 						title: '中级经理人考试',
-						levelColor: 'background-color: lightgreen',
-						backgroundColor: 'background-color: #55ff7f'
+						levelColor: 'background-color: #e58b1c',
+						backgroundColor: 'background-color: #f2c48b',
+						pass:'1'
 					},
 					{
-						levelCode:'2',
+						levelCode: '2',
 						levelName: '高级',
 						title: '高级经理人考试',
-						levelColor: 'background-color: gray',
-						backgroundColor: 'background-color: #aa55ff'
+						levelColor: 'background-color: #27b148',
+						backgroundColor: 'background-color: #b4e4bf',
+						pass:'0'
 					},
 				]
 			}
 		},
-		methods:{
-			linkTo(val){
-				alert('跳转对应考试页面'+val)
+		methods: {
+			linkTo(val,pass) {
+				if(pass=='1'){
+					alert('跳转对应考试页面' + val)
+				}
+				
 			},
 			backTo() {
 				uni.navigateBack({
-			
+
 				})
 			}
 		}
@@ -65,25 +75,44 @@
 <style lang="scss" scoped>
 	#studyExamination {
 		background-color: #ffffff;
+
 		.title {
 			text-align: center;
 		}
 
+		.brage {
+			width: 100rpx;
+			height: 50rpx;
+			border: 4rpx solid #dd4e44;
+			text-align: center;
+			padding-top: 4rpx;
+			color: #dd4e44;
+			position: relative;
+			float: right;
+			top: 26rpx;
+			left: 6rpx;
+			background-color: #FFFFFF;
+			font-size: 20rpx;
+			transform: rotate(-40deg);
+			-ms-transform: rotate(-40deg);/* IE 9 */
+			-webkit-transform: rotate(-40deg);/* Safari and Chrome */
+		}
+
 		.se-col {
-			// background-color: #FCBD71;
-			height: 260rpx;
-			margin-left: 36rpx !important;
+			height: 280rpx;
+			margin-left: 40rpx !important;
 			margin-top: 14rpx;
 			border-radius: 12rpx;
+
 			.se-text {
-				width: 140rpx;
+				width: 150rpx;
 				height: 140rpx;
 				text-align: center;
-				// background-color: orange;
 				border-radius: 12rpx;
-				padding-top: 60rpx;
-				margin-left: 74rpx;
-				margin-top: 40rpx;
+				padding-top: 50rpx;
+				margin: 80rpx 0 0 74rpx;
+				color: #fff;
+				font-size: 32rpx;
 			}
 		}
 	}
