7690dc2d26981b818fae345576b6103e7340f619
fushichang <15311115825@126.com>
2020-11-10 15:09:19 +0800

    修改

--- jiyi-h5/pages/tabbar/index/index.vue
+++ jiyi-h5/pages/tabbar/index/index.vue
@@ -116,8 +116,8 @@ export default {
 			// alert("从app获取用户id=" + e.U + " sessionId=" + e.SI)
 			// Interface.common.userId = e.U; //缓存用户id
 			// Interface.common.sessionId = e.SI; //缓存sessionId
-			// localStorage.setItem('userId', e.U);
-			localStorage.setItem('userId', '20200909');
+			localStorage.setItem('userId', e.U);
+			// localStorage.setItem('userId', '20200909');
 			localStorage.setItem('sessionId', e.SI);
 			let us = localStorage.getItem('userId');
 			let ss = localStorage.getItem('sessionId');
